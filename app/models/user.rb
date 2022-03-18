a5b07aa5eb1882c861631937ca5b98614d8792e4
bay-kuy <keeper.4mr@gmail.com>
2022-03-18 15:05:33 +0900

    userテーブルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :name, :school, :department, presence: true
+         validates :grade, numericality: { only_integer: true }, presence: { message: "can't be blank" }
 end
