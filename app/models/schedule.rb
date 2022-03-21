9451816bb18994664e11fa2b954855e3b3c7efcf
bay-kuy <keeper.4mr@gmail.com>
2022-03-21 15:10:02 +0900

    scheduleモデルにバリデーション設定

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,12 @@
 class Schedule < ApplicationRecord
   belongs_to :user
+
+  validates :start, :end, presence: true
+  validate  :start_end_check
+  
+  def start_end_check
+    if self.start_time.present? && self.end_time.present?
+      errors.add(:end, "が開始時刻を上回っています。正しく記入してください。") if self.start > self.end 
+    end
+  end
 end
