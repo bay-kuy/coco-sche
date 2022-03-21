08f7d45a14a18f99490b5d5ead8b0f37020a939c
bay-kuy <keeper.4mr@gmail.com>
2022-03-21 16:25:12 +0900

    Update schedules_controller.rb

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,4 +2,12 @@ class SchedulesController < ApplicationController
   def index
     @schedules = Schedule.includes(:user)
   end
+  def new
+    @schedule = Schedule.new
+  end
+
+  private
+  def params_event
+    params.require(:schedule).permit(:title, :start, :end, :place, :description)
+  end
 end
